03ed8a6279d6939788c0b0a949abf35d97bc8c4b
Alurienara <78911975+Alurienara@users.noreply.github.com>
2023-01-19 14:12:28 +0500

    Допил смены изображения по таймеру

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,59 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.ActivityMainBinding
-import kotlin.random.Random
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var imageUrl: String
+    //   execute on the main thread, empty constructor is deprecated
+    //    инициировали объект класса Handler
+    private lateinit var handler: Handler
+    //    инициировали блок выполняемого кода
+    private lateinit var runnableCode: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        //hide status bar
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN )
-
-
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater).also {setContentView(it.root)}
+        //hide status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-        imageUrl = savedInstanceState?.getString(KEY_IMAGE_URL) ?: imagesList[Random.nextInt(imagesList.size)]
+//      пересоздали явно объект класса Handler и запускаем его в основном потоке
+        handler = Handler(Looper.getMainLooper())
 
-        Glide.with(this)
-            .load(imageUrl)
-            .into(binding.imageView)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        //    счетчик для перебора списка
+        var index = 0
+
+//      пересоздали явно объект класса Runnable
+        runnableCode = Runnable {
+//          путь к картинкам (массиву). Можно заменить на единственную ссылку
+//            imageUrl = savedInstanceState?.getString(KEY_IMAGE_URL) ?: imagesList[Random.nextInt(
+//                imagesList.size
+//            )]
+            imageUrl = savedInstanceState?.getString(KEY_IMAGE_URL) ?: imagesList[index]
+            index+=1
+            if (index == imagesList.size) {
+                index = 0
+            }
+//          Работаем с glide для вывода на экран картинки (см выше)
+            Glide.with(this)
+                .load(imageUrl)
+                .into(binding.imageView)
+//          обращаемся к объекту класса handler и с задержкой в [2] (второй аргумент в мс)
+//          выполняем функцию runnableCode (рекурсия?). 900 000 мс = 15 минут.
+            handler.postDelayed(runnableCode, 2000)
+        }
+//      запуск вышенаписанного кода
+        handler.post(runnableCode)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -38,13 +64,14 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val KEY_IMAGE_URL = "KEY_IMAGE_URL"
 
-        val imagesList:List<String> = listOf(
+//        url картинок
+        val imagesList: List<String> = listOf(
             "https://fotorelax.ru/wp-content/uploads/2016/12/Beautiful-nature-of-Canadas-landscapes-Mark-Jinks-23.jpg",
             "https://crosti.ru/patterns/00/0d/c6/0d_picture_20df317f.jpg",
-            "https://img-fotki.yandex.ru/get/97201/127908635.1418/0_1ac2f7_fa876a0_orig.jpg",
-            "http://192.168.1.100/wall.jpg"
+            "https://img-fotki.yandex.ru/get/97201/127908635.1418/0_1ac2f7_fa876a0_orig.jpg"
         )
-
+//        заменяем все url выше на локальную ссылку или правим путь
+//        "http://192.168.1.100/wall.jpg"
 //        последняя сссылка - на локальный сервер с картинкой, остальные - пример работы (кажется не все отображаются, возможно косячные ссылки)
     }
 }
